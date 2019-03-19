68fb658f9f5caf741436fc52a3016adb35ae320b
root <root@daw2m08-ivtogo.fjeclot.net>
2019-03-19 17:41:44 +0100

    Commit 1

---
+++ peupagina.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Peu de pàgina per defecte per a noves entrades 
+Plugin URI: https://github.com/ivtogo1819daw2/pluginwp2_ivator.git
+Description: Crea peus de pàgina per defecte per a noves entrades
+Version: 0.1
+Author: Ivan Torres
+Author URI: https://github.com/ivtogo1819daw2
+License: GNU GPLv3 
+*/ 
+?>
