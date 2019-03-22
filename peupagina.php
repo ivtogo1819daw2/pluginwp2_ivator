a1229793f44973692663a0d0770c4a30e9bbf108
root <root@daw2m08-ivtogo.fjeclot.net>
2019-03-22 13:58:27 +0100

    Commit 2

--- peupagina.php
+++ peupagina.php
@@ -7,5 +7,15 @@ Version: 0.1
 Author: Ivan Torres
 Author URI: https://github.com/ivtogo1819daw2
 License: GNU GPLv3 
-*/ 
+*/
+
+function admin_menu_peu_pagina(){ 
+$titolPagina="Peu de pàgina per defecte";
+$titolEntrada="Peu de pàgina per defecte";
+$role="administrator";
+$id="peu_pagina_per_defecte";
+add_menu_page($titolPagina,$titolEntrada,$role,$id); ;
+}
+add_action('admin_menu','admin_menu_peu_pagina');
+ 
 ?>
